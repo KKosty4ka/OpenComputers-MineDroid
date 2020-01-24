3fb010ca7a281f655ee40a025e3d3c70ccea8cca
KKosty4ka <49340075+KKosty4ka@users.noreply.github.com>
2020-01-24 15:23:06 +0700

    Update buttons.lua

--- lib/buttons.lua
+++ lib/buttons.lua
@@ -24,49 +24,49 @@ local tButtons = {
 --    }
 }
 
-local function buttons.setButtons(dict)
+function buttons.setButtons(dict)
 	tButtons = dict
 end
 
-local function buttons.drawButton(n) -- функция рисования кнопки
+local function drawButton(n) -- функция рисования кнопки
     gpu.setBackground(tButtons[n].color) -- задаем цвет кнопки
     gpu.setForeground(tButtons[n].textColor) -- задаем цвет текста
     gpu.fill(tButtons[n].X, tButtons[n].Y, tButtons[n].W, tButtons[n].H, ' ') -- заливаем область
     gpu.set(tButtons[n].X+(tButtons[n].W/2)-(#tButtons[n].text/2), tButtons[n].Y+(tButtons[n].H/2), tButtons[n].text) -- пишем текст по центру
 end
 
-local function buttons.toggleVisible(n) -- переключение видимости кнопки
+local function toggleVisible(n) -- переключение видимости кнопки
     if tButtons[n].visible then -- если кнопка видима
         tButtons[n].visible = false -- отключаем
         gpu.setBackground(b_color) -- берем цвет фона, полученный при старте программы
         gpu.fill(tButtons[n].X, tButtons[n].Y, tButtons[n].W, tButtons[n].H, ' ') -- стираем кнопку
     else -- если кнопка не активна
         tButtons[n].visible = true -- активируем
-        buttons.drawButton(n) -- запускаем отрисовку
+        drawButton(n) -- запускаем отрисовку
     end
 end
 
-local function buttons.blink(n) -- мигание кнопки
+local function blink(n) -- мигание кнопки
     tButtons[n].color, tButtons[n].textColor = tButtons[n].textColor, tButtons[n].color -- меняем местами цвета фона и текста
-    buttons.drawButton(n) -- отрисовываем кнопку
+    drawButton(n) -- отрисовываем кнопку
     os.sleep(0.09) -- делаем задержку
     tButtons[n].color, tButtons[n].textColor = tButtons[n].textColor, tButtons[n].color -- меняем цвета обратно
-    buttons.drawButton(n) -- перерисовываем кнопку
+    drawButton(n) -- перерисовываем кнопку
 end
 
 gpu.fill(1, 1, W, H, ' ') -- очищаем экран
 
 for i = 1, #tButtons do
-    buttons.toggleVisible(i) -- активируем каждую кнопку
+    toggleVisible(i) -- активируем каждую кнопку
 end
 
-local function buttons.main()
+function buttons.main()
 	while true do
 		local tEvent = {pull_e('touch')} -- ждем клика
 		for i = 1, #tButtons do -- перебираем все кнопки
 			if tButtons[i].visible then -- если кнопка активна
 				if tEvent[3] >= tButtons[i].X and tEvent[3] <= tButtons[i].X+tButtons[i].W and tEvent[4] >= tButtons[i].Y and tEvent[4] <= tButtons[i].Y+tButtons[i].H then -- если клик произведен в пределах кнопки
-				buttons.blink(i) -- мигнуть кнопкой
+				blink(i) -- мигнуть кнопкой
 				tButtons[i].action() -- выполнить назначенный код
 				break
 				end
